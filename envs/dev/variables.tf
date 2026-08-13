3376367b15469560d3c7c0fabed3402e22c4ee79
tomasevicnikola <ntomasevic@griddynamics.com>
2026-08-13 22:03:23 +0200

    test: break fmt

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,17 +1,17 @@
 variable "project_id" {
   description = "GCP project that owns every resource in this environment."
-  type        = string
+  type = string
   default     = "petclinic-capstone"
 }
 
 variable "region" {
   description = "Default region for regional resources."
-  type        = string
+  type = string
   default     = "europe-west3"
 }
 
 variable "zone" {
   description = "Default zone for zonal resources."
-  type        = string
+  type = string
   default     = "europe-west3-a"
 }
