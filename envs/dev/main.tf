9e3891e518e7cc4351483c7fd8fe591fc1f8f7f9
tomasevicnikola <ntomasevic@griddynamics.com>
2026-08-15 21:17:14 +0200

    chore: wire cloudsql into envs/dev

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,5 +1,6 @@
 locals {
   ops_vm_service_account = "sa-ops-vm@${var.project_id}.iam.gserviceaccount.com"
+  app_vm_service_account = "sa-app-vm@${var.project_id}.iam.gserviceaccount.com"
 }
 
 module "network" {
@@ -25,4 +26,28 @@ module "ops_vm" {
   service_account_email = local.ops_vm_service_account
 
   network_tags = [module.network.tags.ssh_iap, module.network.tags.ops]
+}
+
+module "cloudsql" {
+  source = "../../modules/cloudsql"
+
+  project_id = var.project_id
+  region     = var.region
+
+  instance_name      = var.db_instance_name
+  network_self_link  = module.network.network_self_link
+  allocated_ip_range = module.network.psa_range_name
+
+  database_name    = var.db_name
+  app_user_name    = var.db_app_user
+  password_version = var.db_password_version
+
+  tier                = var.db_tier
+  availability_type   = var.db_availability_type
+  deletion_protection = var.db_deletion_protection
+
+  secret_prefix             = var.db_secret_prefix
+  app_service_account_email = local.app_vm_service_account
+
+  depends_on = [module.network]
 }
